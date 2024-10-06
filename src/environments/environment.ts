df701d7b9e7a7e82ce77edfd3aec28637bbd003f
SamT-dev <samson.nzumgueng@outlook.com>
2024-10-06 12:56:17 +0200

    feat: add link api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_BASE_URL:'https://api-server-waj.cyclic.app/'
+    API_BASE_URL:'https://server-xmasf.onrender.com'
     //API_BASE_URL: 'http://localhost:9000'
 };
